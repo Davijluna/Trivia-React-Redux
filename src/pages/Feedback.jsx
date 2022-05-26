8eeb2b26a5a199cf4ad54b2d357f3b4b38bfd7c7
davi <davijluna@gmail.com>
2022-05-26 15:51:16 -0300

    requisito 14

--- src/pages/Feedback.jsx
+++ src/pages/Feedback.jsx
@@ -1,14 +1,48 @@
 import React from 'react';
 import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
+
+import Hearder from '../components/Header';
 
 class Feedback extends React.Component {
+  FeedbackMessage = () => {
+    const { assertions } = this.props;
+    const condition = 3;
+    if (assertions < condition) {
+      return 'Could be better...';
+    }
+    return 'Well Done!';
+  };
+
   render() {
+    const { score, assertions } = this.props;
     return (
+
       <div>
-        <h1 data-testid="feedback-text">Feedback</h1>
+        <Hearder />
+        <h2 data-testid="feedback-text">{ this.FeedbackMessage() }</h2>
+
+        <div>
+          Pontuação:
+          <span data-testid="feedback-total-score">{ score }</span>
+        </div>
+        <div>
+          <span data-testid="feedback-total-question">{ assertions }</span>
+        </div>
       </div>
+
     );
   }
 }
 
-export default connect()(Feedback);
+const mapStateToProps = (state) => ({
+  score: state.player.score,
+  assertions: state.player.assertions,
+});
+
+Feedback.propTypes = {
+  assertions: PropTypes.number.isRequired,
+  score: PropTypes.number.isRequired,
+};
+
+export default connect(mapStateToProps)(Feedback);
