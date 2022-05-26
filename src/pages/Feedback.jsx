ef244db33df605dc85b790d6e6ae018e27c92746
Frederico Leles Santos <fred.leles@hotmail.com>
2022-05-26 15:29:56 -0300

    Req 15

--- src/pages/Feedback.jsx
+++ src/pages/Feedback.jsx
@@ -1,14 +1,30 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 
 class Feedback extends React.Component {
+  handlePlayAgain = () => {
+    const { history } = this.props;
+    history.push('/');
+  };
+
   render() {
     return (
       <div>
         <h1 data-testid="feedback-text">Feedback</h1>
+        <button
+          data-testid="btn-play-again"
+          onClick={ this.handlePlayAgain }
+        >
+          Play Again
+        </button>
       </div>
     );
   }
 }
+
+Feedback.propTypes = {
+  history: PropTypes.shape({ push: PropTypes.func.isRequired }).isRequired,
+};
 
 export default connect()(Feedback);
