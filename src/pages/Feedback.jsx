66f2ff11d689730bbd6cfa2dc6aec2ece10a2896
Felipe Avila <f.avila@hotmail.com.br>
2022-05-26 15:05:02 -0300

    Ajustado requisito 11 - incluido  header

--- src/pages/Feedback.jsx
+++ src/pages/Feedback.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import { connect } from 'react-redux';
+import Header from '../components/Header';
 
 class Feedback extends React.Component {
   render() {
     return (
       <div>
         <h1 data-testid="feedback-text">Feedback</h1>
+        <Header />
       </div>
     );
   }
