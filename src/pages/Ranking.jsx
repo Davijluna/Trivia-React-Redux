bdd10670530d80485149ba2fe939fb25cbf513f7
Lucas da Cunha Moreti <lucasdacunha00@gmail.com>
2022-05-26 16:42:38 -0300

    Requisito 18

--- src/pages/Ranking.jsx
+++ src/pages/Ranking.jsx
@@ -4,11 +4,24 @@ import { connect } from 'react-redux';
 import Header from '../components/Header';
 
 class Ranking extends React.Component {
+  goToHome = () => {
+    const { history } = this.props;
+    history.push('/');
+  }
+
   render() {
     return (
       <div>
         <Header />
         <h1 data-testid="ranking-title">Feedback</h1>
+        <button
+          data-testid="btn-go-home"
+          type="button"
+          onClick={ this.goToHome }
+        >
+          Início
+
+        </button>
       </div>
     );
   }
